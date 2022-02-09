0819ecb55c26d57496f6b5836db38f0e247738ea
U16A0-U16B9 <xxvirginslayer420xx@gmail.com>
2022-02-09 21:39:47 +0100

    feature | dotenv

--- config.py
+++
@@ -1,9 +0,0 @@
-SHODAN_API_KEY = "Insert your API key here"
-SCTRAILS_API_KEY = "Insert your API key here"
-BOT_TOKEN = "Insert your bot token here"
-API_URL = "Insert your tgsint-api url here"
-
-#Insert your telegram user id/s
-users = [
-    1234567890,
-]
