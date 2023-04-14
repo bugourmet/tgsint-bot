e5d6d986f659b886902d51fbe7b85cd0809c1bb9
runtimeterrorist <67608248+runtimeterrorist@users.noreply.github.com>
2023-04-15 01:11:14 +0200

    linter & changelog

--- main.py
+++ main.py
@@ -1,59 +1,51 @@
-from telegram.ext import Updater, CommandHandler,Filters
-import logging
 import os
+import logging
+from telegram.ext import Updater, CommandHandler, Filters
 from dotenv import load_dotenv
-import modules.shodan.lookup as shodan
-import modules.nmap.lookup as nmap
 import modules.person.lookup as person
 import modules.cars.lookup as cars
-import modules.nmap.lookup as nmap
 
 
-#logging.basicConfig(filename='log.txt', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
-load_dotenv() 
+load_dotenv()
 
 
-def error(update, context): 
-    logger.warning("%s" ' caused error: "%s"', update, context.error)
-    
+def error(update, context):
+    """error logger"""
+    logger.error("%s caused error: %s", update, context.error)
 
-def help(update, context):
+
+def helpmsg(update, context):
+    """Bot help function,displays help message when called"""
     update.message.reply_text("""
       *Available bot commands:*\n
       /find - Person lookup by name.
       /phone - Person lookup by phone number.
-      /whois - WHOIS lookup.
-      /cve - CVE scan using shodan.
-      /domains - Subdomains scan. 
-      /geoip - Lookup target geoip info.
       /bihreg - Lookup bosnian car license plates.
       /croreg - Lookup croatian car license plates.
-      /nmap - Nmap scans.
-    """,parse_mode= 'Markdown')
+    """, parse_mode='Markdown')
 
 
 def main() -> None:
-    # Create the updater and pass it your bot's token.
+    """Main bot function, creates the updater and registers command handlers"""
     bot = Updater(os.environ.get("BOT_TOKEN"))
     users = list(map(int, os.environ.get("USERS").split('|')))
 
-    # Get the dispatcher to register handlers.
     dispatcher = bot.dispatcher
     dispatcher.add_error_handler(error)
-    dispatcher.add_handler(CommandHandler("phone", person.phone, Filters.user(user_id=users)))
-    dispatcher.add_handler(CommandHandler("whois", person.whois, Filters.user(user_id=users)))
-    dispatcher.add_handler(CommandHandler("cve", shodan.cvescan, Filters.user(user_id=users)))
-    dispatcher.add_handler(CommandHandler("bihreg", cars.bihreg, Filters.user(user_id=users)))
-    dispatcher.add_handler(CommandHandler("croreg", cars.croreg, Filters.user(user_id=users)))
-    dispatcher.add_handler(CommandHandler("find", person.find, Filters.user(user_id=users)))
-    dispatcher.add_handler(CommandHandler("geoip", shodan.geoip, Filters.user(user_id=users)))
-    dispatcher.add_handler(CommandHandler("domains", nmap.domains, Filters.user(user_id=users)))
-    dispatcher.add_handler(CommandHandler("nmap", nmap.nmap_scan, Filters.user(user_id=users)))
-    dispatcher.add_handler(CommandHandler("help", help, Filters.user(user_id=users)))
+    dispatcher.add_handler(CommandHandler(
+        "phone", person.phone, Filters.user(user_id=users)))
+    dispatcher.add_handler(CommandHandler(
+        "find", person.find, Filters.user(user_id=users)))
+    dispatcher.add_handler(CommandHandler(
+        "bihreg", cars.bihreg, Filters.user(user_id=users)))
+    dispatcher.add_handler(CommandHandler(
+        "croreg", cars.croreg, Filters.user(user_id=users)))
+    dispatcher.add_handler(CommandHandler(
+        "help", helpmsg, Filters.user(user_id=users)))
 
-    # Start the bot.
     bot.start_polling()
     bot.idle()
 
